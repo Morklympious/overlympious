4241013a6b82122affc6ff30c094af513a65dd47
Morklympious <morklympious@gmail.com>
2022-11-17 01:17:36 -0800

    Add TV frame, center it, add dynamic brb messages

--- src/routes/brb/+page.svelte
+++ src/routes/brb/+page.svelte
@@ -16,13 +16,16 @@
     let context;
 
     const gogogo = () => {
-        if(!video || !window.obsstudio) {
+        if(!video) {
             return;
         }
 
         vidsrc = anything();
+        console.log("yo?", video, vidsrc);
 
-        requestAnimationFrame(() => (video.muted = false));
+        if(!window.obsstudio) {
+          return;
+        }
 
         context = new AudioContext();
         const source = context.createMediaElementSource(video);
@@ -57,6 +60,8 @@
         reverb.connect(lowpass);
         lowpass.connect(compressor);
         compressor.connect(context.destination);
+
+        video.play();
     };
 
 
@@ -73,16 +78,17 @@
 
         <div class="frames">
             {#if vidsrc}
-            <TelevisionFrame>
-              <div class="video crt">
-                <!-- svelte-ignore a11y-media-has-caption -->
-                <video width="640px" class="video" on:ended={() => (gogogo())} autoplay controls bind:this={video}>
-                    <source class="crt" src="{anything()}" />
-                </video>
+            <div class="television">
+              <TelevisionFrame>
+                <div class="video crt">
+                  <!-- svelte-ignore a11y-media-has-caption -->
+                  <video src="{vidsrc}" width="640px" class="video" on:ended={() => (gogogo())} autoplay controls bind:this={video} />
+                </div>
+
+              </TelevisionFrame>
+              <h1 class="note"> MORKLYMPIOUS ENTERTAINMENT NETWORK </h1>
             </div>
-            </TelevisionFrame>
-            
-             {/if}
+            {/if}
         </div>
 
     </div>
@@ -106,8 +112,8 @@
 
     grid-template:
         "neon neon neon . " 1fr
-        "frames frames frames ." 1fr
-        "frames frames frames ." 1fr
+        "frames frames frames frames" 1fr
+        "frames frames frames frames" 1fr
         "corner . . ." 12vh / 1fr 1fr 1fr 1fr;
 
     position: relative;
@@ -127,12 +133,24 @@
 .frames {
     display: flex;
     align-items: center;
-
+    justify-content: center;
     grid-area: frames;
 }
 
 .video {
   position: relative;
+}
+
+.television {
+  position: absolute;
+}
+.note {
+  text-align: center;
+  opacity: 0.4;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 1.1rem;
 }
 
 .crt::before {
@@ -230,89 +248,10 @@
 }
 
 
-@keyframes swing {
-    0% { transform: rotate(3deg); }
-    100% { transform: rotate(-3deg); }
-}
-
-
-.swing {
-    animation: swing ease-in-out 3s infinite alternate;
-    transform-origin: center -20px;    
-}
-
-@keyframes textShadow {
-  0% {
-    text-shadow: 0.4389924193300864px 0 1px rgba(0,30,255,0.5), -0.4389924193300864px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  5% {
-    text-shadow: 2.7928974010788217px 0 1px rgba(0,30,255,0.5), -2.7928974010788217px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  10% {
-    text-shadow: 0.02956275843481219px 0 1px rgba(0,30,255,0.5), -0.02956275843481219px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  15% {
-    text-shadow: 0.40218538552878136px 0 1px rgba(0,30,255,0.5), -0.40218538552878136px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  20% {
-    text-shadow: 3.4794037899852017px 0 1px rgba(0,30,255,0.5), -3.4794037899852017px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  25% {
-    text-shadow: 1.6125630401149584px 0 1px rgba(0,30,255,0.5), -1.6125630401149584px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  30% {
-    text-shadow: 0.7015590085143956px 0 1px rgba(0,30,255,0.5), -0.7015590085143956px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  35% {
-    text-shadow: 3.896914047650351px 0 1px rgba(0,30,255,0.5), -3.896914047650351px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  40% {
-    text-shadow: 3.870905614848819px 0 1px rgba(0,30,255,0.5), -3.870905614848819px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  45% {
-    text-shadow: 2.231056963361899px 0 1px rgba(0,30,255,0.5), -2.231056963361899px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  50% {
-    text-shadow: 0.08084290417898504px 0 1px rgba(0,30,255,0.5), -0.08084290417898504px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  55% {
-    text-shadow: 2.3758461067427543px 0 1px rgba(0,30,255,0.5), -2.3758461067427543px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  60% {
-    text-shadow: 2.202193051050636px 0 1px rgba(0,30,255,0.5), -2.202193051050636px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  65% {
-    text-shadow: 2.8638780614874975px 0 1px rgba(0,30,255,0.5), -2.8638780614874975px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  70% {
-    text-shadow: 0.48874025155497314px 0 1px rgba(0,30,255,0.5), -0.48874025155497314px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  75% {
-    text-shadow: 1.8948491305757957px 0 1px rgba(0,30,255,0.5), -1.8948491305757957px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  80% {
-    text-shadow: 0.0833037308038857px 0 1px rgba(0,30,255,0.5), -0.0833037308038857px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  85% {
-    text-shadow: 0.09769827255241735px 0 1px rgba(0,30,255,0.5), -0.09769827255241735px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  90% {
-    text-shadow: 3.443339761481782px 0 1px rgba(0,30,255,0.5), -3.443339761481782px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  95% {
-    text-shadow: 2.1841838852799786px 0 1px rgba(0,30,255,0.5), -2.1841838852799786px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-  100% {
-    text-shadow: 2.6208764473832513px 0 1px rgba(0,30,255,0.5), -2.6208764473832513px 0 1px rgba(255,0,80,0.3), 0 0 3px;
-  }
-}
-
 .crt {
-  animation: textShadow 1.4s infinite;
   overflow: hidden;
 
   width: 100%;
-  /* height: 30rem; */
     
   display: flex;
   justify-content: center;
